602821f7e6a61e96506ae692940bc631f6e142fc
Tyler Gannon <tgannon@gmail.com>
2011-05-17 23:40:36 -0700

    More

--- public/javascripts/jquery.serialize_page_elements.js
+++ public/javascripts/jquery.serialize_page_elements.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
   (function( $ ){
     var serious_methods = {
       text_area: function($el) {
-        console.log('ele:');
-        console.log($el);
         return {html: $el.find('.contents').html()};
       },
       page_element: function($el) {
@@ -15,11 +13,8 @@ $(document).ready(function() {
     $.fn.serialize_page_elements = function() {
       return $.map(this.children(), 
         function(elementOfArray, indexInArray) { 
-          console.log(elementOfArray);
           $el = $(elementOfArray);
-          console.log($el.attr('id'));
           idInfo = /^(.*)_([^_]+)$/.exec($el.attr('id'));
-          console.log(idInfo);
           elementType = idInfo[1]
           
           return {
